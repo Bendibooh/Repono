9bfeb3b93309aba3d2859a1499a7a48b5520efd1
Bendibooh <nagy.benedek.b@gmail.com>
2022-10-16 16:58:46 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>sdaasdasdads
+          Edit <code>src/App.js</code>dsdsaads
         </p>
         <a
           className="App-link"
