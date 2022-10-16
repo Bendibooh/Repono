5dfc0e57b0a1e183a4e73ffff3538629b6832539
Bendibooh <nagy.benedek.b@gmail.com>
2022-10-16 17:23:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import TopBanner from './components/TopBanner/TopBanner';
+import Services from './components/Services/Services';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>dsdsaads
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <TopBanner />
+    <Services />
+  </div>
   );
 }
 
