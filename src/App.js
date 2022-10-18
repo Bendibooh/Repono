a8a9d33fdae31ef96f94fe069588805ae67b199c
Bendibooh <nagy.benedek.b@gmail.com>
2022-10-18 14:48:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Services from './components/Services/Services';
 import TopNavigation from './components/TopNavigation/TopNavigation';
 import Charts from './components/Charts/Charts';
 import Summary from './components/Summary/Summary';
+import RecentProject from './components/RecentProject/RecentProject';
+import Courses from './components/Courses/Courses';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Services />
     <Charts/>
     <Summary/>
+    <RecentProject/>
+    <Courses />
   </div>
   );
 }
