37e9870e1778334beeeb20068a3994411a8ab098
Bendibooh <nagy.benedek.b@gmail.com>
2022-11-12 21:08:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import {BrowserRouter as Router} from "react-router-dom"
-import AppRouter from './router/AppRouter'
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import AppRouter from './router/AppRouter';
+import Loading from './components/Loading/Loading';
 
 function App() {
+  
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1200)
+  }, [])
   return (
+    <>
+    {loading === false ? (
     <div>
      <Router>
      <AppRouter />
      </Router>
   </div>
+        ) : (
+          <Loading />
+        )}
+      </>
   );
 }
 
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,25 @@
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import LoaderIcon from '../../asset/image/loader2.svg';
+
+class Loading extends Component {
+     render() {
+          return (
+       
+               <Fragment>
+                    <Container className="text-center">
+                         <Row>
+                              <Col>
+                              <object type="image/svg+xml" data={LoaderIcon} className="loader" alt="React Logo" />
+                              </Col>
+                         </Row>
+                    </Container>
+
+               </Fragment>
+
+
+          )
+     }
+}
+
+export default Loading
